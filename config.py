122d698655d3010732f77d61b75b6f134a8f3d57
wangui.njoka <wangui.njoka@gmail.com>
2018-09-17 13:40:08 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = 'smtp.googlemail.com'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wanguinjoka:hakiizzy@localhost/personalblog'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")

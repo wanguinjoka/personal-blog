6d3500ceaa0af3e6647a27498d09f4f795f1ae23
wangui.njoka <wangui.njoka@gmail.com>
2018-09-17 15:40:51 +0300

    deployment

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wanguinjoka:hakiizzy@localhost/personalblog'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wanguinjoka:hakiizzy@localhost/personalblog'
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_PUCE_URL")
+    
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wanguinjoka:hakiizzy@localhost/personalblog'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wanguinjoka:hakiizzy@localhost/personalblog'
     DEBUG = True
 
 
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY='flaskkey123'
+export SECRET_KEY=flaskkey123
 
 export MAIL_USERNAME=wangui.njoka@gmail.com
 export MAIL_PASSWORD=hakiizzy
